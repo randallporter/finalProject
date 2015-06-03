67b959609ef43e4aa2c43a3ec12628a794b945b5
Random Davis <random.davis@gmail.com>
2015-06-02 18:46:26 -0700

    Updated flow file

---
+++ tests/mapping_test.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from source.mapFile import Mapper, Mapping
+import os
+import datetime
+import getpass
+import random
+import socket
+import subprocess
+import threading
+import time
+import logging
+
+
+class TestMapper(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_
