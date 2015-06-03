9aa0f3c505ad0b10ef1676ede9cf0d9256f300bf
Randall Porter <porterrandall@yahoo.com>
2015-06-02 19:19:17 -0700

    added trans

--- source/importingFile.py
+++ source/importingFile.py
@@ -1,6 +1,9 @@
 import os
 
-def getCSVS(providedPath):
-    for fileName in os.listdir(providedPath):
-     if os.path.isfile(fileName):
-        print (fileName)
+def get_csvs(provided_path):
+    file_list = {}
+    index = 1
+    for fileName in os.listdir(provided_path):
+        if '.csv' in fileName:
+            file_list[index] = fileName
+    return file_list
