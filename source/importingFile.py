d719358203891855684aac01a8613ab44988c26f
Random Davis <random.davis@gmail.com>
2015-06-03 17:26:31 -0700

    drone.io breaking still?

--- source/importingFile.py
+++ source/importingFile.py
@@ -1,11 +1,15 @@
 import os
 
-def get_csvs(provided_path):
+def get_csvs(provided_path, files):
     file_list = []
+    index = 0
     if os.path.exists(provided_path):
         for fileName in os.listdir(provided_path):
             if '.csv' in fileName:
                 file_list.append(fileName)
+            index += 1
     else:
         raise Exception(provided_path + " does not exist.")
+    if index != files:
+        raise Exception("only " + str(index) + " files in directory")
     return file_list
--- tests/import_test.py
+++ tests/import_test.py
@@ -21,12 +21,23 @@ class TestImport(TestCase):
         if not os.path.exists(self.test_dir):
             os.makedirs(self.test_dir)
 
-        with open(self.test_dir + "test1.csv", "w+") as f1:
-            f1.write("test,test,test")
-        with open(self.test_dir + "test2.csv", "w+") as f2:
-            f2.write("test,test,test")
-        with open(self.test_dir + "test3.csv", "w+") as f3:
-            f3.write("test,test,test")
+        #with open(self.test_dir + "test1.csv", "w+") as f1:
+        #    f1.write("test,test,test")
+        #with open(self.test_dir + "test2.csv", "w+") as f2:
+        #    f2.write("test,test,test")
+        #with open(self.test_dir + "test3.csv", "w+") as f3:
+        #    f3.write("test,test,test")
+
+        f1 = open(self.test_dir + "test1.csv", "wb")
+        f1.write("test,test,test")
+        f1.close()
+        f2 = open(self.test_dir + "test2.csv", "wb")
+        f2.write("test,test,test")
+        f2.close()
+        f3 = open(self.test_dir + "test3.csv", "wb")
+        f3.write("test,test,test")
+        f3.close()
+
 
         self.correct_file_list = ["test1.csv", "test2.csv", "test3.csv"]
 
@@ -36,15 +47,15 @@ class TestImport(TestCase):
         os.rmdir(self.test_dir)
 
     def test_find_three_csv(self):
-        self.assertEqual(self.correct_file_list, sorted(get_csvs(self.test_dir)))
+        self.assertEqual(self.correct_file_list, sorted(get_csvs(self.test_dir, 3)))
 
     def test_find_no_csv(self):
         empty_dir = ".\\nothing_here\\"
         if not os.path.exists(empty_dir):
             os.makedirs(empty_dir)
-        self.assertEqual([], get_csvs(empty_dir))
+        self.assertEqual([], get_csvs(empty_dir, 0))
         os.rmdir(empty_dir)
 
     def test_bad_path(self):
         with self.assertRaises(Exception):
-            get_csvs(".\\asdasdasdasd\\")
+            get_csvs(".\\asdasdasdasd\\", 0)
